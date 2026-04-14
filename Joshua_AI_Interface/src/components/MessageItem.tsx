6ca7c31d9a376e2086d43df7959923403210b652
Joshua Eme <joshuaeme3@gmail.com>
2026-04-14 05:35:05 +0100

    Icons added and text clean up

--- Joshua_AI_Interface/src/components/MessageItem.tsx
+++ Joshua_AI_Interface/src/components/MessageItem.tsx
@@ -1,18 +1,23 @@
 // Individual bubbles (styles differ for "User" vs. "Assistant")
 import "../index.css"
 import type { Message } from "../type";
+import systemIcon from "../assets/system-icon.jpg";
+import userIcon from "../assets/user-icon.jpg";
 
 function MessageItem(message: {message: Message}){
     const { content, sender, timestamp } = message.message;
+
     // Add an icon to determine who is user and who is system
     if (sender == "system"){ 
         return( <div className="message-item-system">
-        <h3>{content}</h3>
+            <img src={systemIcon} alt="System Icon" className="system-icon" />
+            <p>{content}</p>
         </div>)
     } 
 
-    return(<div className="message-item-user">
-        <h3>{content}</h3>
+    return(<div className="message-item-user">           
+            <p>{content}</p>
+            <img src={userIcon} alt="User Icon" className="user-icon" />
         </div>
     )
 }
